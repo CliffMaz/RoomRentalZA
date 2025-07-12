6597b3928dd8fa434468e164eb23ac2c0c299242
CliffMaz <cliff.mz18@gmail.com>
2025-07-12 18:51:57 +0200

    database mockup file

---
+++ assets/Database.js
@@ -0,0 +1,242 @@
+
+
+const rentalData = [
+    {
+      id: 1,
+      type: "Room",
+      room_dimensions:'1350m x 900m',
+      price: 1250,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat:-26.100039,
+        lng: 27.772207
+      }
+    },
+    {
+      id: 2,
+      type: "Cottage",
+      room_dimensions:'1350m x 900m',
+      price: 1250,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat: -26.095163,
+        lng: 27.770355
+      }
+    },
+    {
+      id: 3,
+      type: "Cottage",
+      room_dimensions:'1350m x 900m',
+      price: 3050,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat:-26.102571,
+        lng: 27.766827
+      }
+    },
+    {
+      id: 4,
+      type: "Room",
+      room_dimensions:'1350m x 900m',
+      price: 950,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat:-26.097994,
+        lng: 27.770685
+      }
+    },
+    {
+      id: 5,
+      type: "Room",
+      room_dimensions:'1350m x 900m',
+      price: 1250,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat:-26.098656,
+        lng: 27.772744
+      }
+    },
+    {
+      id: 6,
+      type: "Cottage",
+      room_dimensions:'1350m x 900m',
+      price: 3250,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat:-26.101142,
+        lng: 27.777107
+      }
+    },
+    {
+      id: 7,
+      type: "Room",
+      room_dimensions:'1350m x 900m',
+      price: 1750,
+      parking: true,
+      deposit: true,
+      address: "224 veld street",
+      cell: 670173408,
+      images:[
+         {
+        id: 1,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:2,
+        pic: require('../../assets/hous.jpg')
+    },
+    {
+        id: 3,
+        pic: require('../../assets/homescreen.png'),
+    },
+
+    {
+        id:4,
+        pic: require('../../assets/hous.jpg')
+    }
+      ],
+      coord:{
+        lat:-26.100960,
+        lng: 27.775332
+      }
+    },
+  ];
